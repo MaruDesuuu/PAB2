24ed3efe41230791ed940c1ad19788ec3c50643c
MaruDesuuu <mariostv270204@mhs.mdp.ac.id>
2024-03-08 20:37:27 +0700

    adding things

--- pilem/lib/screens/favorite_screen.dart
+++ pilem/lib/screens/favorite_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
 import 'package:pilem/screens/detail_screen.dart';
@@ -36,29 +35,35 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _loadFavoriteMovies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorite Screen'),
-      ),
+      appBar: AppBar(title: const Text('Favorite Screen')),
       body: ListView.builder(
         itemCount: _favoriteMovies.length,
         itemBuilder: (context, index) {
           final Movie movie = _favoriteMovies[index];
           return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            padding: const EdgeInsets.symmetric(vertical: 4),
             child: ListTile(
               leading: Image.network(
-                'https//image.tmdb.org/t/p/w500${movie.posterPath}',
+                'https://image.tmdb.org/t/p/w500${movie.posterPath}',
                 height: 50,
                 width: 50,
+                fit: BoxFit.cover,
               ),
               title: Text(movie.title),
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                      builder: (context) => DetailScreen(movie: movie)),
+                    builder: (context) => DetailScreen(movie: movie),
+                  ),
                 );
               },
             ),
